5948cde77a127382e85240c11756b7e30a147ce7
DashtagGovender <85450046+DashtagGovender@users.noreply.github.com>
2024-02-19 06:38:39 +0200

    Adding back terms and privacy

--- index.js
+++ index.js
@@ -15,39 +15,13 @@ app.post("/tab", (req, res) => {
   res.render(`${req.body.choice}.ejs`);
 });
 
-// app.get("/services", (req, res) => {
-//   res.render("services.ejs");
-// });
+app.get("/terms", (req, res) => {
+  res.render("terms.ejs");
+});
 
-// app.get("/solutions", (req, res) => {
-//   res.render("solutions.ejs");
-// });
-
-// app.get("/clients", (req, res) => {
-//   res.render("clients.ejs");
-// });
-
-// app.get("/social", (req, res) => {
-//   res.render("social.ejs");
-// });
-
-// app.get("/contact", (req, res) => {
-//   res.render("contact.ejs");
-// });
-
-// app.get("/terms", (req, res) => {
-//   res.render("terms.ejs");
-// });
-
-// app.get("/privacy", (req, res) => {
-//   res.render("privacy.ejs");
-// });
-
-// app.get("/tab", (req, res) => {
-//   const anchor = req.body["Anchor"];
-//   console.log("anchor");
-//   res.render(`${anchor.toLowerCase()}.ejs`);
-// });
+app.get("/privacy", (req, res) => {
+  res.render("privacy.ejs");
+});
 
 app.listen(process.env.PORT || port, () => {
   console.log(`Server running on port ${port}`);
